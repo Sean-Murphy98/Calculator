78e1b6c7c8f576799d9d56666a0300dcf55a8de9
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-09-06 05:51:48 -0700

    Basic operation control function

--- source.js
+++ source.js
@@ -13,3 +13,18 @@ function multiply(num1, num2){
 function divide(num1, num2){
     console.log(num1/num2)
 }
+
+function operate(operator, num1, num2){
+    if (operator == "+"){
+        add(num1, num2)
+    }
+    else if (operator == "-"){
+        subtract(num1, num2)
+    }
+    else if (operator == "x"){
+        multiply(num1, num2)
+    }
+    if (operator == "÷"){
+        divide(num1, num2)
+    }
+}
