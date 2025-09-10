e048815b86c8ebf0cb57c51e71760b60d8d738f7
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-09-10 03:11:47 -0700

    Subtraction implementation on click

--- source.js
+++ source.js
@@ -1,11 +1,10 @@
 function add(num1, num2){
-    console.log(num1)
-    console.log(num2)
     return(num1 + num2);
 }
 
 function subtract(num1, num2){
-    console.log(num2 - num1)
+    console.log(num1 - num2)
+    return (num1 - num2)
 }
 
 function multiply(num1, num2){
@@ -22,7 +21,7 @@ function operate(operator, num1, num2){
        result = add(num1, num2)
     }
     else if (operator == "-"){
-        subtract(num1, num2)
+       result = subtract(num1, num2)
     }
     else if (operator == "x"){
         multiply(num1, num2)
@@ -43,12 +42,28 @@ function figDisplay(fig){
     display.textContent += fig;
 }
 
+function operHandler(oper, val1, val2){
+    ans = operate(oper, parseInt(val1), parseInt(val2));
+    console.log(ans)
+    clearDisplay();
+    figDisplay(ans);
+    val1 = ans;
+    val2 = "";
+    return [val1, val2]
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     let newCalc = true;
     let entrystr = "";
+    let val1 = "";
+    let val2 = "";
+    let oper = "";
     const clearButton = document.querySelector('#btnClear')
     clearButton.addEventListener('click', () => {
         clearDisplay();
+        val1 = "";
+        val2 = "";
+        oper = "";
         newCalc = true;
     })
     for (let i=0; i<10; i++){
@@ -59,24 +74,50 @@ document.addEventListener('DOMContentLoaded', function(){
                 newCalc = false;
             }
             figDisplay(i)
+            if (!oper){
+                val1 += i
+            }
+            else{
+                val2 += i
+            }
         })
     }
     let plusButton = document.querySelector('#btnAdd');
     plusButton.addEventListener('click', () => {
-        if (!newCalc){
+        if ((newCalc && val1) || !newCalc && !val2 && !oper){
             figDisplay(" + ");
+            oper = "+";
+            newCalc = false;
+        }
+        else if (val2){
+            vals = operHandler(oper, val1, val2)
+            val1 = vals[0]
+            val2 = vals[1]
+            figDisplay(" + ")
+        }
+    })
+    let minusButton = document.querySelector('#btnSub');
+    minusButton.addEventListener('click', () => {
+        if ((newCalc && val1) || !newCalc && !val2 && !oper){
+            figDisplay(" - ");
+            oper = "-";
+            newCalc = false;
+        }
+        else if (val2){
+            vals = operHandler(oper, val1, val2)
+            val1 = vals[0]
+            val2 = vals[1]
+            figDisplay(" - ")
         }
     })
     eqButton = document.querySelector('#btnEq')
     eqButton.addEventListener('click', () => {
-        if (!newCalc) {
-            const display = document.querySelector("#display");
-            entrystr = display.textContent;
-            operateStr = entrystr.replace(/\s/g, "").split(/([+\-x÷])/);
-            console.log(operateStr);
-            ans = operate(operateStr[1],operateStr[0],operateStr[2]);
-            clearDisplay();
-            figDisplay(ans);
+        if (!newCalc && val2) {
+            vals = operHandler(oper, val1, val2)
+            val1 = vals[0]
+            val2 = vals[1]
+            oper = "";
+            newCalc = true;
         }
     })
 });
